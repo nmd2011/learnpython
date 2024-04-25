f10d7c94273f70f52a5a70f9d18776cee0a64143
fyznitch <fyznitch@gmail.com>
2024-04-24 20:48:27 -0700

    created project2 and company site

--- day_22_project/project_22.py
+++ day_22_project/project_22.py
@@ -1,26 +1,30 @@
 import streamlit as st
 import pandas as pd
-
+st.set_page_config(layout='wide')
 df = pd.read_csv("data.csv")
 
-print(df)
+st.title("The Best Company Ever")
+st.text("""Random thoughts of nothing but how to fill the space in the page and in my head"
+        "Hoping this is enough babbling to cover more of the page""")
+
+st.header("THE TEAM")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     for index, row in df[:4].iterrows():
-        st.write(f"{row['first name']} {row['last name']}")
+        st.subheader(f"{row['first name']} {row['last name']}".title())
         st.write(row["role"])
-        st.image("images/" + row["image"])
+        st.image("imgs/" + row["image"])
 
     with col2:
         for index, row in df[4:8].iterrows():
-            st.write(f"{row['first name']} {'last name'}")
+            st.subheader(f"{row['first name']} {row['last name']}".title())
+            st.write(row["role"])
+            st.image("imgs/" + row["image"])
 
     with col3:
         for index, row in df[8:].iterrows():
-            st.write(f"{row['first name']} {'last name'}")
-
-
-
-
+            st.subheader(f"{row['first name']} {row['last name']}".title())
+            st.write(row["role"])
+            st.image("imgs/" + row["image"])
