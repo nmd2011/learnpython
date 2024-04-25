0e6cf02bc136655576ca8e16538e75ac9719a620
fyznitch <fyznitch@gmail.com>
2024-04-24 21:38:21 -0700

    create contact me form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email form"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your Message")
+    submit_button = st.form_submit_button("Submit")
+    if submit_button:
+        print("button clicked")
