e8a0c6de654238133fd2af2c7843b878782b40cf
fyznitch <fyznitch@gmail.com>
2024-04-27 09:19:31 -0700

    find repo

---
+++ send_email.py
@@ -0,0 +1,22 @@
+import smtplib, ssl  # standard email library
+
+host = 'smtp.gmail.com'
+port = 465
+test = 5655
+subject= "Email subject"
+username = 'fyznitch@gmail.com'
+password = 'xxhndnakhwcmkzrs'
+recipient = "fyznitch@gmail.com"
+message = f"""\
+Subject: {subject}
+df sdkhjdlgf sdgfksdgj sdgkldgjf dfgdfgkljdf gdfgkjdf g
+dsjfkds gdfkgjd fgjdklgfj dlfgjdfkg dfgkjdf gdfjgdlgf jdfg
+jdsfgkl dsgjfkldjgflkdgjf dlkgjkdlg jdlkgj dlgfj dlkgjdfgj 
+dfjg dfkgjdlfgjdklgj dlkgjf ldkgjflkdgjfldkgjf dfjgdlfg j
+dfgj ldkgjf kdlgjfkl djgfkldjfglkdgjf lkdjfgkldjfgkljdkg d
+"""
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, recipient, message)
